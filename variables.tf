ce937959661c1603967406bcf3578bcf0b8e482b
Tobi Popoola <opopoola@eminenttechnology.com>
2022-10-03 12:00:36 +0100

    latest commit

---
+++ variables.tf
@@ -0,0 +1,25 @@
+# Sets global variables for this Terraform project.
+
+// also note that 
+
+variable app_name {
+}
+
+variable location {
+  default = "West US"
+}
+
+variable admin_username {
+  default = "linux_admin"
+}
+
+variable client_id {
+
+}
+
+variable client_secret {
+
+}
+
+
+//setting the 
